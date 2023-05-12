fe9cee09d8cfc52f15ddc48b6858de3df5bf790b
Alexander_Leon <alexanderleon.pacheco@gmail.com>
2023-05-12 10:51:54 -0500

    as

---
+++ validar.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "carrito";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+
+$usuario = $_POST['usuario'];
+$contraseña = $_POST['contraseña'];
+
+$sql = "SELECT * FROM usuario WHERE usuario = '$usuario' AND contraseña = '$contraseña'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // El usuario y la contraseña son válidos
+    echo "Inicio de sesión exitoso. ¡Bienvenido!";
+} else {
+    // El usuario o la contraseña son incorrectos
+    echo "Usuario o contraseña incorrectos. Por favor, intenta nuevamente.";
+}
+
+$conn->close();
+?>
